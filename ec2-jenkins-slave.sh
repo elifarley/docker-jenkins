bdde82055d32b132f4178fcc777085d135f4e537
Elifarley C <elifarley@users.noreply.github.com>
2016-12-19 13:02:33 -0200

    Update ec2-jenkins-slave.sh

--- ec2-jenkins-slave.sh
+++ ec2-jenkins-slave.sh
@@ -12,7 +12,7 @@ apt-get install mercurial
 
 id
 
-mkdir -p ~/.ssh || exit
+mkdir -p ~/.ssh ~/.docker || exit
 
 cat <<-EOF >> ~/.hgrc || exit
 [ui]
@@ -29,6 +29,7 @@ chmod 0644 ~/.ssh/authorized_keys ~/.ssh/known_hosts || exit
 
 hg clone 'ssh://hg@bitbucket.org/elifarley/m4u.jenkins-slave.config' ~/jenkins-slave.config || exit
 
-cp -av gradle.properties ~/.gradle/gradle.properties
+cp -av ~/jenkins-slave.config/docker-config.json ~/.docker/config.json && \
+cp -av ~/jenkins-slave.config/gradle.properties ~/.gradle/gradle.properties || exit
 
 sudo JAVA_HOME="${JAVA_HOME:-/usr/lib/jvm/default-jvm}" /usr/local/bin/keytool-import-certs --force ~/jenkins-slave.config/mnt-ssh-config/certs
