8a3c9939dd03585117f306352be74b35404dec7f
Elifarley C <elifarley@users.noreply.github.com>
2016-12-20 15:08:53 -0200

    Update ec2-jenkins-slave.sh

--- ec2-jenkins-slave.sh
+++ ec2-jenkins-slave.sh
@@ -14,7 +14,7 @@ id
 
 sudo ln -s ~/app /app || exit
 
-mkdir -p ~/app ~/.ssh ~/.gradle ~/.docker || exit
+mkdir -p ~/app ~/.ssh || exit
 
 cat <<-EOF >> ~/.hgrc || exit
 [ui]
@@ -35,9 +35,20 @@ chmod 0700 ~/.ssh && \
 chmod 0400 ~/.ssh/* && \
 chmod 0644 ~/.ssh/authorized_keys ~/.ssh/known_hosts || exit
 
-hg clone 'ssh://hg@bitbucket.org/elifarley/m4u.jenkins-slave.config' ~/jenkins-slave.config || exit
+# --
 
-cp -av ~/jenkins-slave.config/docker-config.json ~/.docker/config.json && \
-cp -av ~/jenkins-slave.config/gradle.properties ~/.gradle/gradle.properties || exit
+HG_URL='ssh://hg@bitbucket.org/elifarley/m4u.jenkins-slave.config'
+
+if test -d ~/jenkins-slave.config/.hg; then
+  hg --cwd ~/jenkins-slave.config pull && hg --cwd ~/jenkins-slave.config up -C
+else
+  echo "Cloning repository '$HG_URL' to '$HOME/jenkins-slave.config'..."
+  hg clone "$HG_URL" ~/jenkins-slave.config || exit
+fi
+
+mkdir -p ~/.m2 ~/.gradle ~/.docker && ( cd ~/jenkins-slave.config && \
+cp -av mvn-settings.xml ~/.m2/settings.xml && \
+cp -av gradle.properties ~/.gradle/gradle.properties && \
+cp -av docker-config.json ~/.docker/config.json ) || exit
 
 sudo JAVA_HOME="${JAVA_HOME:-/usr/lib/jvm/default-jvm}" /usr/local/bin/keytool-import-certs --force ~/jenkins-slave.config/mnt-ssh-config/certs
