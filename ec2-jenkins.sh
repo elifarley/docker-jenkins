862278ee100e60f600c76be956eb48e35670e754
Elifarley C <elifarley@users.noreply.github.com>
2016-12-20 10:35:37 -0200

    Fix tilde expansion

--- ec2-jenkins.sh
+++ ec2-jenkins.sh
@@ -8,47 +8,51 @@
 
 #---
 
+set -x
+
 _USER="${_USER:-admin}"
 _COMPANY="${_COMPANY:-company}"
 
-mkdir -p ~"$_USER"/.ssh ~"$_USER"/bin
+_HOME="$(getent passwd "$_USER" | cut -d: -f 6)"
 
-cat <<-EOF >> ~"$_USER"/.hgrc || exit
+mkdir -p "$_HOME"/.ssh "$_HOME"/bin
+
+cat <<-EOF >> "$_HOME"/.hgrc || exit
 [ui]
 username = ${_COMPANY} Robot <${_COMPANY}robot@company.com>
 EOF
 
-cat <<-EOF >> ~"$_USER"/.ssh/config || exit
+cat <<-EOF >> "$_HOME"/.ssh/config || exit
 Host bitbucket.org
   IdentityFile ~/.ssh/${_COMPANY}robot@bitbucket.pem
   IdentitiesOnly yes
   User git
 EOF
 
-curl -fsSL -o ~"$_USER"/bin/hgbkp-jenkins.sh \
+curl -fsSL -o "$_HOME"/bin/hgbkp-jenkins.sh \
 https://gist.githubusercontent.com/elifarley/2d1842d9579063e2f3b3fce0516e62ec/raw/63623be878ffbb6f77c490c93a567b403f57d185/hgbkp-jenkins.sh
 
-cat <<-EOF > ~"$_USER"/bin/app-bkp.sh || exit
+cat <<-EOF > "$_HOME"/bin/app-bkp.sh || exit
 #!/bin/bash
 
-time ~"$_USER"/bin/hgbkp-jenkins.sh ~"$_USER"/jenkins-home ssh://hg@bitbucket.org/elifarley/${_COMPANY}.jenkins main
+time "$_HOME"/bin/hgbkp-jenkins.sh "$_HOME"/jenkins-home ssh://hg@bitbucket.org/elifarley/${_COMPANY}.jenkins main
 
 EOF
 
-chmod +x ~"$_USER"/bin/* || exit
+chmod +x "$_HOME"/bin/* || exit
 
-aws s3 --quiet cp s3://${_COMPANY}.jenkins/mnt-ssh-config/known_hosts /dev/stdout | cat >> ~"$_USER"/.ssh/known_hosts && \
-aws s3 cp s3://${_COMPANY}.jenkins.secrets/${_COMPANY}robot@bitbucket.pem ~"$_USER"/.ssh/ || exit
+aws s3 --quiet cp s3://${_COMPANY}.jenkins/mnt-ssh-config/known_hosts /dev/stdout | cat >> "$_HOME"/.ssh/known_hosts && \
+aws s3 cp s3://${_COMPANY}.jenkins.secrets/${_COMPANY}robot@bitbucket.pem "$_HOME"/.ssh/ || exit
 
-chmod 0700 ~"$_USER"/.ssh && \
-chmod 0400 ~"$_USER"/.ssh/* && \
-chmod 0644 ~"$_USER"/.ssh/authorized_keys ~"$_USER"/.ssh/known_hosts && \
-chown -R "$_USER":"$_USER" ~"$_USER"/.ssh || exit
+chmod 0700 "$_HOME"/.ssh && \
+chmod 0400 "$_HOME"/.ssh/* && \
+chmod 0644 "$_HOME"/.ssh/known_hosts && \
+chown -R "$_USER":"$_USER" "$_HOME"/.ssh || exit
 
-sudo -u "$_USER" ~"$_USER"/bin/app-bkp.sh || exit
+sudo -u "$_USER" "$_HOME"/bin/app-bkp.sh || exit
 
 echo "*/5 * * * *   ~'$_USER'/bin/app-bkp.sh" | crontab - -u "$_USER" || exit
 
-ln -s ~"$_USER"/jenkins-home/custom-config/bin/docker-jenkins.sh ~"$_USER"/bin/app.sh || exit
+ln -s "$_HOME"/jenkins-home/custom-config/bin/docker-jenkins.sh "$_HOME"/bin/app.sh || exit
 
-exec sudo -u "$_USER" ~"$_USER"/bin/app.sh
+exec sudo -u "$_USER" "$_HOME"/bin/app.sh
