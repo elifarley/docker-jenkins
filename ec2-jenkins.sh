2d0657f3e4da67f422290fbb7f8e4cd340814dfb
Elifarley C <elifarley@users.noreply.github.com>
2016-12-21 17:04:13 -0200

    Update ec2-jenkins.sh

--- ec2-jenkins.sh
+++ ec2-jenkins.sh
@@ -30,7 +30,7 @@ Host bitbucket.org
 EOF
 
 curl -fsSL -o "$_HOME"/bin/hgbkp-jenkins.sh \
-https://gist.githubusercontent.com/elifarley/2d1842d9579063e2f3b3fce0516e62ec/raw/b61a23a5353a0ce504300a0f3285b0f4603028e2/hgbkp-jenkins.sh
+https://gist.githubusercontent.com/elifarley/2d1842d9579063e2f3b3fce0516e62ec/raw/ecbe2ecc31e0f6056b03bdd9887469e779049e61/hgbkp-jenkins.sh
 
 cat <<-EOF > "$_HOME"/bin/app-bkp.sh || exit
 #!/bin/bash
