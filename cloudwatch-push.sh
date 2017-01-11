c590a0f139779d06e9e69904b5b9ba78998540cf
Elifarley C <elifarley@users.noreply.github.com>
2017-01-11 18:52:24 -0200

    Parse log timestamp (depends on Jenkins timestamp plugin)

--- cloudwatch-push.sh
+++ cloudwatch-push.sh
@@ -8,7 +8,7 @@ strcontains() { test -z "${1##*$2*}" ; }
 strcontains "$UPSTREAM_JOB_NAME" '/' && \
 UPSTREAM_JOB_NAME="$(dirname "$UPSTREAM_JOB_NAME")/job/$(basename "$UPSTREAM_JOB_NAME")"
 
-curl -fsSL "${JENKINS_URL}job/$UPSTREAM_JOB_NAME/$UPSTREAM_BUILD_NUMBER/consoleText" | \
+curl -fsSL "${JENKINS_URL}job/$UPSTREAM_JOB_NAME/$UPSTREAM_BUILD_NUMBER/timestamps/?time=HH:mm:ss&appendLog" | \
 aws logs push \
 --log-group-name /jenkins/jobs \
 --log-stream-name "$STREAM_NAME" \
