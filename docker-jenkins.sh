aa9b16acf44ac9e14460407c3e1e187784debaaf
Elifarley C <elifarley@users.noreply.github.com>
2017-02-03 18:11:12 -0200

    Update docker-jenkins.sh

--- docker-jenkins.sh
+++ docker-jenkins.sh
@@ -7,16 +7,29 @@ set -x
 docker pull "$IMAGE"
 
 curl -fsL --connect-timeout 1 http://169.254.169.254/latest/meta-data/local-ipv4 >/dev/null && {
+  hostname="$(hostname)"
+  log_stream_name="$(date +'%Y%d%m-%H%M%S') $(echo ${hostname%%.*} ${IMAGE##*:} | tr -s ':*' ';.')"
   log_config="
   --log-driver=awslogs
   --log-opt awslogs-group=/jenkins/master
-  --log-opt awslogs-stream='$(hostname)/$(basename "$IMAGE")@$(date -Is)'
+  --log-opt awslogs-stream=$log_stream_name
   "
   cp -av ~/.ssh/*.p?? "$CMD_BASE"/../mnt-ssh-config/
+  echo "Log stream name: $log_stream_name"
 }
 
 #--log-opt awslogs-region=sa-east-1 \
 # 
+
+dimg() { docker inspect "$1" |grep Image | grep -v sha256: | cut -d'"' -f4 ;}
+dstatus() { docker inspect "$1" | grep Status | cut -d'"' -f4 ;}
+
+test "$(dstatus jenkins 2>/dev/null)" = running && {
+  echo "OLD IMAGE: $(dimg jenkins)"
+  docker stop jenkins
+}
+
+docker rm jenkins
 
 exec docker run --name jenkins \
 -p 8080:8080 -p 50000:50000 -p 9910:9910 -p 9911:9911 \
