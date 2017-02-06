eb4af44614a6d10620e091b25d14c912b9b2e752
Elifarley C <elifarley@users.noreply.github.com>
2017-02-06 11:59:15 -0200

    Better CloudWatch stream name

--- docker-jenkins.sh
+++ docker-jenkins.sh
@@ -3,19 +3,18 @@ CMD_BASE="$(readlink -f "$0")" || CMD_BASE="$0"; CMD_BASE="$(dirname "$CMD_BASE"
 
 IMAGE="elifarley/docker-jenkins-uidfv:2-latest"
 
-set -x
 docker pull "$IMAGE"
 
 curl -fsL --connect-timeout 1 http://169.254.169.254/latest/meta-data/local-ipv4 >/dev/null && {
   hostname="$(hostname)"
-  log_stream_name="$(date +'%Y%d%m-%H%M%S') $(echo ${hostname%%.*} ${IMAGE##*:} | tr -s ':*' ';.')"
+  log_stream_name="$(date +'%Y%d%m.%H%M%S');$(echo ${hostname%%.*}/${IMAGE##*:} | tr -s ':* ' ';..')"
   log_config="
   --log-driver=awslogs
   --log-opt awslogs-group=/jenkins/master
   --log-opt awslogs-stream=$log_stream_name
   "
-  cp -av ~/.ssh/*.p?? "$CMD_BASE"/../mnt-ssh-config/
   echo "Log stream name: $log_stream_name"
+  cp -av ~/.ssh/*.p?? "$CMD_BASE"/../mnt-ssh-config/
 }
 
 #--log-opt awslogs-region=sa-east-1 \
@@ -24,14 +23,21 @@ curl -fsL --connect-timeout 1 http://169.254.169.254/latest/meta-data/local-ipv4
 dimg() { docker inspect "$1" |grep Image | grep -v sha256: | cut -d'"' -f4 ;}
 dstatus() { docker inspect "$1" | grep Status | cut -d'"' -f4 ;}
 
-test "$(dstatus jenkins 2>/dev/null)" = running && {
-  echo "OLD IMAGE: $(dimg jenkins)"
-  docker stop jenkins
-}
+drun() {
+  local name="$1"; test $# -gt 0 && shift
+  local status="$(dstatus "$name" 2>/dev/null)"; echo "Container status for '$name': $status"
 
-docker rm jenkins
+  case "$status" in running|restarting|created)
+    echo "OLD IMAGE: $(dimg "$name")"
+    echo "DOWNTIME START: $(date)"
+    docker stop >/dev/null "$name" && docker >/dev/null rm "$name" || exit
+  ;; exited) echo "DOWNTIME START: $(date)"; docker >/dev/null rm "$name" || exit
+  ;; '') echo "Container '$name' not found."
+  ;; *) echo "Unknown container status: $status"; docker ps | grep "$name"; echo "DOWNTIME START: $(date)"; docker rm -f "$name"
+  esac
 
-exec docker run --name jenkins \
+  ( set -x
+docker run --name "$name" \
 -p 8080:8080 -p 50000:50000 -p 9910:9910 -p 9911:9911 \
 --dns=10.11.64.21 --dns=10.11.64.22 --dns-search=m4ucorp.dmc \
 -v "$CMD_BASE"/../..:/var/jenkins_home \
@@ -46,4 +52,10 @@ exec docker run --name jenkins \
 -Djava.rmi.server.hostname=$(curl -fsL --connect-timeout 1 http://169.254.169.254/latest/meta-data/local-ipv4 || hostname)" \
 -d --restart=always \
 $log_config \
-"$IMAGE" "$@"
+"$IMAGE" "$@" || return
+  ) || return
+
+  echo "DOWNTIME END: $(date)"
+}
+
+drun jenkins
