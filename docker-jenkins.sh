9b3da84c76428036888c7a26d31dd4bb43e702ff
Elifarley C <elifarley@users.noreply.github.com>
2017-04-06 11:25:07 -0300

    Update docker-jenkins.sh

--- docker-jenkins.sh
+++ docker-jenkins.sh
@@ -21,9 +21,6 @@ curl -fsL --connect-timeout 1 http://169.254.169.254/latest/meta-data/local-ipv4
 dimg() { docker inspect "$1" |grep Image | grep -v sha256: | cut -d'"' -f4 ;}
 dstatus() { docker inspect "$1" | grep Status | cut -d'"' -f4 ;}
 
-#--log-opt awslogs-region=sa-east-1 \
-#
-
 drun() {
   local name="$1"; test $# -gt 0 && shift
   local status="$(dstatus "$name" 2>/dev/null)"; echo "Container status for '$name': $status"
@@ -37,10 +34,11 @@ drun() {
   ;; *) echo "Unknown container status: $status"; docker ps | grep "$name"; docker rm -f "$name"
   esac
 
+# --dns=10.11.64.21 --dns=10.11.64.22 --dns-search=m4ucorp.dmc \
+
   ( set -x
   docker run -d --restart=always --name "$name" \
 -p 8080:8080 -p 50000:50000 -p 9910:9910 -p 9911:9911 \
---dns=10.11.64.21 --dns=10.11.64.22 --dns-search=m4ucorp.dmc \
 -v "$CMD_BASE"/../..:/var/jenkins_home \
 -v "$CMD_BASE"/../mnt-ssh-config:/mnt-ssh-config:ro \
 -e JENKINS_OPTS="--prefix=/jenkins" \
